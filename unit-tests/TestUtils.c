1171a2ad64195208500d4a1dc7158721f37cfc21
Frederic Jarlier <fjarlier@yahoo.fr>
2019-03-14 16:26:22 +0100

    Update TestUtils.c

--- unit-tests/TestUtils.c
+++ unit-tests/TestUtils.c
@@ -1,3 +1,28 @@
+/*
+   mpiSORT
+   Copyright (C) 2016-2019 Institut Curie / Institut Pasteur
+   mpiSORT is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   mpiSORT is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   You should have received a copy of the GNU Lesser Public License
+   along with mpiSORT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+   Module:
+     TestUtils.c
+     
+   Authors:
+    Frederic Jarlier,   Institut Curie
+    Firmin Martin,      Paris Descartes
+   
+*/
+
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
