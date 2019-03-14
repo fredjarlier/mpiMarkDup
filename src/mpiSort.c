08e34cdb9ad06c782b9002e0b13803600f7bf49b
Frederic Jarlier <fjarlier@yahoo.fr>
2019-03-14 15:36:02 +0100

    Update mpiSort.c

--- src/mpiSort.c
+++ src/mpiSort.c
@@ -1,6 +1,6 @@
 /*
    mpiSORT
-   Copyright (C) 2016-2017 Institut Curie / Institut Pasteur
+   Copyright (C) 2016-2019 Institut Curie / Institut Pasteur
 
    mpiSORT is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -27,6 +27,7 @@
     Leonor Sirotti,     Institut Curie
     Thomas Magalhaes,   Institut Curie
     Paul Paganiban,     Institut Curie
+    Firmain Martin,     Paris Descartes University
 */
 
 #ifdef HAVE_CONFIG_H
